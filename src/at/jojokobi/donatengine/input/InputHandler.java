625f9fa3a9c7f209ae299a343d3b6f43c9f90380
DatDonathan <jojo08082003@gmail.com>
2020-04-21 17:23:15 +0200

    Added getInputs() method in InputHandler

--- src/at/jojokobi/donatengine/input/InputHandler.java
+++ src/at/jojokobi/donatengine/input/InputHandler.java
@@ -1,5 +1,9 @@
 package at.jojokobi.donatengine.input;
 
+import java.util.Arrays;
+import java.util.List;
+
+import at.jojokobi.donatengine.util.Pair;
 import at.jojokobi.netutil.server.Server;
 
 @FunctionalInterface
@@ -8,6 +12,10 @@ public interface InputHandler {
 	public static final long SCENE_INPUT = Server.BROADCAST_CLIENT_ID;
 
 	public Input getInput(long clientId);
+	
+	public default List<Pair<Long, Input>> getInputs () {
+		return Arrays.asList(new Pair<>(SCENE_INPUT, getInput(SCENE_INPUT)));
+	}
 	
 	public default Input getInput () {
 		return getInput(SCENE_INPUT);
--- src/at/jojokobi/donatengine/input/MapInputHandler.java
+++ src/at/jojokobi/donatengine/input/MapInputHandler.java
@@ -1,6 +1,10 @@
 package at.jojokobi.donatengine.input;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+
+import at.jojokobi.donatengine.util.Pair;
 
 public class MapInputHandler implements InputHandler {
 
@@ -17,6 +21,18 @@ public class MapInputHandler implements InputHandler {
 	@Override
 	public Input getInput(long clientId) {
 		return clientId == SCENE_INPUT ? local : map.get(clientId);
+	}
+	
+	@Override
+	public List<Pair<Long, Input>> getInputs() {
+		List<Pair<Long, Input>> inputs = new ArrayList<Pair<Long,Input>>();
+		inputs.add(new Pair<Long, Input>(SCENE_INPUT, local));
+		for (var e : map.entrySet()) {
+			if (!e.getKey().equals(SCENE_INPUT)) {
+				inputs.add(new Pair<>(e.getKey(), e.getValue()));
+			}
+		}
+		return InputHandler.super.getInputs();
 	}
 	
 }
