3f916e6d1e20d528f14d8086c3d448bf132f194e
DatDonathan <jojo08082003@gmail.com>
2020-04-22 21:44:58 +0200

    Added getScreenPosition() in CameraHandler

--- src/at/jojokobi/donatengine/rendering/CameraHandler.java
+++ src/at/jojokobi/donatengine/rendering/CameraHandler.java
@@ -3,9 +3,13 @@ package at.jojokobi.donatengine.rendering;
 import at.jojokobi.donatengine.level.Level;
 import at.jojokobi.donatengine.objects.Camera;
 import at.jojokobi.donatengine.objects.GameObject;
+import at.jojokobi.donatengine.util.Vector2D;
+import at.jojokobi.donatengine.util.Vector3D;
 
 public interface CameraHandler {
 	
 	public void doCameraFollow (GameObject follow, Level level, Camera cam, double maxBorderDst);
+	
+	public Vector2D getScreenPosition (Vector3D pos, Camera cam);
 
 }
